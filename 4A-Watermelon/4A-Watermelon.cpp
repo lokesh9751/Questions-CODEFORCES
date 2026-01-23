d2fc4a0989ec372b4abe1c696b0a86cb4241a88e
lokesh9751 <lokeshharbola350@gmail.com>
2026-01-23 22:57:01 +0530

    Added Important Syntax for Javascript

---
+++ 4A-Watermelon/4A-Watermelon.cpp
@@ -0,0 +1,24 @@
+#define _ ios_base::sync_with_stdio(0);cin.tie(0);
+#include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
+int main()
+{
+    int w;
+    cin>>w;
+    if (w > 2)
+    {
+        if (w % 2 == 0)
+        {
+            cout<<"YES";
+        }
+        else
+        {
+            cout<<"YES";
+        }
+    }
+    else
+    {
+        cout<<"NO";
+    }
+}
