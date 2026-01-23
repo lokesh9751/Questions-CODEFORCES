677718fced183f8cef56af288de1d894c624e61a
lokesh9751 <lokeshharbola350@gmail.com>
2026-01-23 22:25:52 +0530

    Organised problems in their folders

--- 4A-Watermelon/4A-Watermelon.js
+++ 4A-Watermelon/4A-Watermelon.js
@@ -4,7 +4,7 @@ const input = fs.readFileSync(0, "utf8").trim();
 let w = Number(input);
 if(w>2){
     if(w%2==0){
-        console.log("YES");
+        console.log("YES"); 
     }
     else{
         console.log("NO");
