8369124caec850ef00dc4bce102ce642d9977eb3
lokesh9751 <lokeshharbola350@gmail.com>
2026-01-26 00:35:55 +0530

    Solved 282A-Bit++ Problem

---
+++ 282A-Bit++/282A-Bit++.cpp
@@ -0,0 +1,20 @@
+#define _ ios_base::sync_with_stdio(false); cin.tie(nullptr);
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n,x=0;
+    cin>>n;
+    for(int i=0;i<n;i++){
+        string s;
+        cin>>s;
+        if(s == "X++" || s == "++X"){
+            x++;
+        }
+        else{
+            x--;
+        }
+    }
+    cout<<x;
+    return 0;
+}
